00eb8a1eda92f728841e40d2265b3c43b69c2815
Xianhui Zhu <xz2715@nyu.edu>
2021-06-03 15:34:19 -0400

    make the web crawler able to get the next page

--- main.py
+++ main.py
@@ -21,9 +21,8 @@ try:
     sleep(3)
 
 finally:
-
     # get into the file
-    browser.find_element_by_xpath("//input[@type='text']").send_keys('nyu')
+    browser.find_element_by_xpath("//input[@type='text']").send_keys(Login.term)
     sleep(1)
 
     browser.find_element_by_xpath("//input[@type='text']").send_keys(Keys.RETURN)
@@ -32,48 +31,37 @@ finally:
     browser.find_element_by_xpath("//button[text() = 'People']").click()
     sleep(3)
 
-# try to connect to one person
-# browser.find_element_by_xpath("//span[text() = 'Connect']/..").click()
-# sleep(1)
-#
-# browser.find_element_by_xpath("//span[text() = 'Add a note']/..").click()
-# sleep(1)
-#
-# browser.find_element_by_xpath("//textarea[@name='message']").send_keys('Hello')
-# sleep(1)
-#
-# browser.find_element_by_xpath("//button[@aria-label='Dismiss']").click()
-# sleep(1)
+# number of people invitation sent so far
+i = 0
 
-# Send the message
-# browser.find_element_by_xpath("//span[text() = 'Send']/..").click()
-# sleep(1)
+# instead can use a number to track how many pages have been traversed
+while(1):
+    # try to connect with multiple person
+    # if no people to connect (all invitations sent), go to next page
+    try:
+        people = browser.find_elements_by_xpath("//span[text() = 'Connect']/..")
+        sleep(2)
 
+        for person in people:
+            i += 1
+            print(i)
+            person.click()
+            sleep(1)
+            browser.find_element_by_xpath("//span[text() = 'Add a note']/..").click()
+            sleep(1)
+            browser.find_element_by_xpath("//textarea[@name='message']").send_keys('Hello')
+            sleep(1)
+            browser.find_element_by_xpath("//button[@aria-label='Dismiss']").click()
+            sleep(1)
+            # Send the message
+            # browser.find_element_by_xpath("//span[text() = 'Send']/..").click()
+            # sleep(1)
+    finally:
+        browser.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+        sleep(2)
+        browser.find_element_by_xpath("//button[@aria-label='Next']").click()
+        sleep(5)
 
-# test click next
-# browser.execute_script('window.scrollTo(0, document.body.scrollHeight);')
-# sleep(2)
-# browser.find_element_by_xpath("//button[@aria-label='Next']").click()
-
-
-
-# try to connect to multiple person
-people = browser.find_elements_by_xpath("//span[text() = 'Connect']/..")
-sleep(2)
-
-for person in people:
-    person.click()
-    sleep(1)
-    browser.find_element_by_xpath("//span[text() = 'Add a note']/..").click()
-    sleep(1)
-    browser.find_element_by_xpath("//textarea[@name='message']").send_keys('Hello')
-    sleep(1)
-    browser.find_element_by_xpath("//button[@aria-label='Dismiss']").click()
-    sleep(1)
-#
-# sleep(2)
-# browser.find_element_by_xpath("//button[@aria-label='Next']").click()
-# sleep(1)
 
 
 
