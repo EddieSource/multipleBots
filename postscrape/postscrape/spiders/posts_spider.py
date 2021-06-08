159369e512e3fd40519fdc96ff429f628b998155
Xianhui Zhu <xz2715@nyu.edu>
2021-06-08 16:59:48 -0400

    refine the crawler that crawls the email from websites

--- postscrape/postscrape/spiders/posts_spider.py
+++ postscrape/postscrape/spiders/posts_spider.py
@@ -1,24 +1,30 @@
 import scrapy
 import csv
-import pandas as pd
+
+
+websites = []
+with open('company_url.csv', newline='') as f:
+    reader = csv.reader(f)  # put in all the company websites
+    headings = next(reader)  # remove the header
+    for row in reader:
+        websites.append(row[1])
 
 
 class PostsSpider(scrapy.Spider):
     name = "posts"
-    start_urls = ['','',''] # enter ur email addresses
+    # start_urls = ['https://www.rendelmanlaw.com/', 'https://www.normapotroslawfirm.com/','https://grant.legal/'] # enter ur email addresses
+    start_urls = websites
+    emails = []
+
     def parse(self, response):
-        emails = []
         curs = response.xpath('//body//text()').re(r'[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,3}')
         curs = [[cur] for cur in list(dict.fromkeys(curs))]
-        emails.extend(curs)
-        print(emails)
+        self.emails.extend(curs)
         # write your sample csv file
 
         with open('emails.csv', 'w') as f:
             write = csv.writer(f)
-
-            write.writerows(emails)
-
+            write.writerows(self.emails)
 
 
 
